64f059ac82d2dbb1eab4444fb7b957ba1df45d55
エスマ117 <polllfuentes@gmail.com>
2024-05-09 15:51:56 -0600

    Barra de busqueda obscura

--- ToolCraft.bat
+++ ToolCraft.bat
@@ -4,7 +4,7 @@ call ToolCraftEnv\Scripts\activate.bat
 
 pip install -r requirements.txt >nul 2>&1
 
-start /B python manage.py runserver >nul 2>&1
+start python manage.py runserver >nul 2>&1
 
 timeout /t 5 /nobreak >nul
 
